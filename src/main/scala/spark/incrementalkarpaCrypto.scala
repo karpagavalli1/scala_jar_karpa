9e5723559d091a824a6ccaf95831a2ca6d4e8586
karpagavalli1 <karpagavalli.saravanan@gmail.com>
2023-03-24 16:50:30 +0000

    incrementalkarpaCrypto updated

--- src/main/scala/spark/incrementalkarpaCrypto.scala
+++ src/main/scala/spark/incrementalkarpaCrypto.scala
@@ -8,7 +8,7 @@ object incrementalkarpaCrypto {
     val spark = SparkSession
       .builder()
       .appName("Spark SQL basic example")
-      .config("spark.some.config.option", "some-value")
+      .config("sparkConnection.some.config.option", "some-value")
       .getOrCreate()
 
     val url = "jdbc:postgresql://ec2-13-40-49-105.eu-west-2.compute.amazonaws.com:5432/testdb"
@@ -17,18 +17,17 @@ object incrementalkarpaCrypto {
     properties.setProperty("password", "WelcomeItc@2022")
     properties.put("driver", "org.postgresql.Driver")
 
-    import spark.implicits._
-    import org.apache.spark.sql.functions._
 
-    val spark = SparkSession.builder().appName("JDBCExample").getOrCreate()
+
+    val sparkConnection = SparkSession.builder().appName("JDBCExample").getOrCreate()
 
     // *****************************************************************************************************
 
-    val maxDF1 = spark.sql("select max(ethereum_id) from scalagroup.Ethereum_initialdataframe1").first()
+    val maxDF1 = sparkConnection.sql("select max(ethereum_id) from scalagroup.Ethereum_initialdataframe1").first()
     val cdc1 = maxDF1.get(0)
     val query1 = s"(select * from ethereum1 where cast(ethereum_id as int) > $cdc1) as tb1"
 
-    val df_ethereum = spark.read.jdbc(url, query1, properties)
+    val df_ethereum = sparkConnection.read.jdbc(url, query1, properties)
     df_ethereum.show(false)
     df_ethereum.write.mode(SaveMode.Append).saveAsTable("scalagroup.Ethereum_initialdataframe1")
 
