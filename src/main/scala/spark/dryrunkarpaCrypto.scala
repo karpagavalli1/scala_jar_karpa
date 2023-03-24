9825bbb2df91de72724a9c8b9659d635bd075a8d
karpagavalli1 <karpagavalli.saravanan@gmail.com>
2023-03-24 17:58:54 +0000

    dryrun transformation updated

--- src/main/scala/spark/dryrunkarpaCrypto.scala
+++ src/main/scala/spark/dryrunkarpaCrypto.scala
@@ -26,50 +26,76 @@ object dryrunkarpaCrypto {
     // *****************************************************************************************************
     // Ethereum table Transformations
     println("Ethereum Initial DataFrame")
-    val df_ethereum = spark.read.jdbc(url, "ethereum1", properties)
+    val df_ethereum = spark.read.jdbc(url, "ethereum", properties)
     df_ethereum.show(false)
     // Create Hive Internal table
     df_ethereum.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.Ethereum_InitialDataFrame1")
 
     // *****************************************************************************************************
-    println("Ethereum DataFrame filtered by price > '1.3'")
-    // filter() Transformation = filter the records in an RDD. filtering price > "1.3".
+
+    println("Ethereum DataFrame filtered by 'ethereum_price > 1.3'")
+    // filter() Transformation = filter the records in an RDD. filtering ethereum_price > "1.3".
     val filtered_df_ethereum = df_ethereum.filter($"ethereum_price" > "1.3")
     filtered_df_ethereum.show(false)
-    df_ethereum.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.Ethereum_Filteredbyprice1")
-    //*****************************************************************************************************
-    println("Ethereum DataFrame sortByKey() descending order by price")
+    // Create Hive Internal table
+    filtered_df_ethereum.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.Ethereum_FilteredByPrice1")
+
+    // *****************************************************************************************************
+
+    println("Ethereum DataFrame sortByKey() descending order by ethereum_price")
     // sortByKey() Transformation
     val sorted_df_ethereum = filtered_df_ethereum.orderBy(desc("ethereum_price"))
     sorted_df_ethereum.show(false)
     // Create Hive Internal table
-    sorted_df_ethereum.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.Ethereum_SortedByKeyByPrice1")
-    //*******************************************************************************************************
+    sorted_df_ethereum.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.ethereum_sortedbykeybyprice1")
+
+    // *****************************************************************************************************
+    println("Drop the ethereum_NULL column and Add a new column ethereum_mean_price")
+    val dropped_ethereum_null_column = df_ethereum.drop("ethereum_NULL")
+    // Compute the ethereum_mean_price of the "ethereum_price" column
+    val ethereum_mean_price = df_ethereum.select(mean(col("ethereum_price"))).first().getDouble(0)
+    val mean_price_df_ethereum = dropped_ethereum_null_column.withColumn("ethereum_mean_price", lit(ethereum_mean_price))
+    mean_price_df_ethereum.show(false)
+    mean_price_df_ethereum.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.ethereum_mean_price1")
+
+    // *****************************************************************************************************
     // Bitcoin table Transformations
     println("Bitcoin Initial DataFrame")
-    val df_bitcoin = spark.read.jdbc(url, "bitcoin1", properties)
+    val df_bitcoin = spark.read.jdbc(url, "bitcoin", properties)
     df_bitcoin.show(false)
     // Create Hive Internal table
-    df_bitcoin.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.Bitcoin_InitialDataFrame1")
-    // *****************************************************************************************************
+    df_bitcoin.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.bitcoin_initialdataframe1")
 
+    // *****************************************************************************************************
     println("Bitcoin DataFrame filtered by 'bitcoin_price < 250'")
-    // filter() Transformation = filter the records in an RDD. filtering 'price < 250'.
+    // filter() Transformation = filter the records in an RDD. filtering 'bitcoin_price < 250'.
     val filtered_df_bitcoin = df_bitcoin.filter($"bitcoin_price" < "250")
     filtered_df_bitcoin.show(false)
     // Create Hive Internal table
-    filtered_df_bitcoin.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.Bitcoin_FilteredByPrice1")
-    // *****************************************************************************************************
+    filtered_df_bitcoin.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.bitcoin_filteredbyprice1")
 
+    // *****************************************************************************************************
     println("Bitcoin DataFrame sortByKey() descending order by bitcoin_price")
     // sortByKey() Transformation
     val sorted_df_bitcoin = filtered_df_bitcoin.orderBy(desc("bitcoin_price"))
     sorted_df_bitcoin.show(false)
     // Create Hive Internal table
-    sorted_df_bitcoin.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.Bitcoin_SortedByKeyByPrice1")
+    sorted_df_bitcoin.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.bitcoin_sortedbykeybyprice1")
 
     // *****************************************************************************************************
+    println("Drop the bitcoin_NULL column and Add a new column bitcoin_mean_price")
+    val dropped_bitcoin_null_column = df_bitcoin.drop("bitcoin_NULL")
+    // Compute the bitcoin_mean_price of the "bitcoin_price" column
+    val bitcoin_mean_price = df_bitcoin.select(mean(col("bitcoin_price"))).first().getDouble(0)
+    val mean_price_df_bitcoin = dropped_bitcoin_null_column.withColumn("bitcoin_mean_price", lit(bitcoin_mean_price))
+    mean_price_df_bitcoin.show(false)
+    mean_price_df_bitcoin.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.bitcoin_mean_price1")
 
+    // inner join Ethereum and Bitcoin
+    println("Ethereum Filtered join Bitcoin Filtered")
+    val df_join = filtered_df_ethereum.join(filtered_df_bitcoin, filtered_df_ethereum("ethereum_id") === filtered_df_bitcoin("bitcoin_id"), "inner")
+    df_join.show(false)
+    df_join.write.mode(SaveMode.Overwrite).saveAsTable("scalagroup.bitcoinjoinethereum1")
 
   }
 }
